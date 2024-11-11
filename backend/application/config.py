d164ee90bbf317c16bb30128b97b5d6238533c57
Syamkarni Uppalapati <101443460+syamkarni@users.noreply.github.com>
2024-11-11 22:32:54 +0530

    register api added

---
+++ backend/application/config.py
@@ -0,0 +1,5 @@
+SQLALCHEMY_DATABASE_URI = 'sqlite:///../db_directory/flaskblog.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+SECRET_KEY = 'your_secret_key'  # need to set this #incomplete
+SECURITY_PASSWORD_SALT= 'salt'
